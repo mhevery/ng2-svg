58233b63fa7395687a7c4f849d2ac900e6ac6085
Misko Hevery <misko@hevery.com>
2015-05-14 13:55:09 -0700

    ViewQuery

--- app.ts
+++ app.ts
@@ -7,63 +7,79 @@ import {
     Query, QueryList,
     View, ViewRef, ViewContainerRef, ProtoViewRef,
     bootstrap, forwardRef, ElementRef,
-    onAllChangesDone} from 'angular2/angular2';
-
+    onChange} from 'angular2/angular2';
 
 
 @Directive({
-  selector: '[view-dst]',
-  properties: { 'viewDst': 'viewDst' }
+  selector: '[ng-template-ref]'
+  property: {'ngTemplateRef', 'ngTemplateRef'}
 })
-class ViewDst {
-  constructor(public viewContainer: ViewContainerRef) {
-    console.log('ViewDst');
-  }
+class NgTemplateRef {
+  ngTemplateRef: any;
 
-  set viewDst(templateRef:SrcAxisTemplate) {
-    console.log("ViewDst.templateRef", templateRef);
-    var viewRef:ViewRef = this.viewContainer.create(templateRef.protoView, -1, templateRef.elementRef);
-    // TODO: set locals
+  constructor(public protoView: ProtoViewRef, public elementRef: ElementRef) {
+    console.log('NgTemplateRef');
   }
 }
 
 
 
+
+
+@Directive({
+  selector: '[ng-outlet]',
+  properties: {
+    'ngOutlet': 'ngOutlet',
+    'ngOutletLocals': 'ngOutletLocals'
+  },
+  lifecycle: [onChange]
+})
+class NgOutlet {
+  ngOutletLocals:any;
+  ngOutlet:NgTemplateRef;
+
+  constructor(public viewContainer: ViewContainerRef) {
+    console.log('NgOutlet');
+  }
+
+  onChange() {
+    console.log("NgOutlet.ngTemplateRef", this.ngTemplateRef, this.ngOutletLocals);
+    // todo: Don't destroy the view if ngTemplateRef did not change
+    this.viewContainer.clear();
+    var viewRef:ViewRef = this.viewContainer.create(ngTemplateRef.protoView, -1, ngTemplateRef.elementRef);
+    for(var prop in this.ngOutletLocals) {
+      viewRef.setLocal(prop, this.ngOutletLocals[prop]);
+    }
+  }
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 @Component({
   selector: 'axis'
 })
 @View({
-  directives: [forwardRef(() => SrcAxisTemplate)],
+  directives: [NgTemplateRef],
   template: `
   <svg>
-    <rect *src-axis-template="var width=width; var y=y;"
+    <rect *ng-template-ref
           width="100%" height="5" x="0" y="0"
           style="fill:rgb(255,0,9);stroke-width:3;stroke:rgb(0,0,0)" />
   </svg>`
 })
 class Axis {
-  templateRef: SrcAxisTemplate;
-  constructor() {
+  ngTemplateRefs: QueryList<NgTemplateRef>;
+  constructor(@ViewQuery(NgTemplateRef) ngTemplateRefs:QueryList<NgTemplateRef>) {
     console.log('Axis');
-  }
-
-  addTemplate(srcAxisTemplate: SrcAxisTemplate) {
-    this.templateRef = srcAxisTemplate;
+    this.ngTemplateRefs = ngTemplateRefs;
   }
 }
 
 
-
-@Directive({
-  selector: '[src-axis-template]'
-})
-class SrcAxisTemplate {
-  constructor(axis: Axis, public protoView: ProtoViewRef, public elementRef: ElementRef) {
-    console.log('SrcAxisTemplate');
-    axis.addTemplate(this);
-  }
-}
 
 
 @Component({
@@ -80,13 +96,13 @@ class SrcAxisTemplate {
           style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
     <svg [attr.width]="width" height="10" x="0" [attr.y]="1*height + 5">
       <g *ng-for="var axis of axes; var i=$index" [attr.y]="i*10">
-        <template [view-dst]="axis.templateRef"></template>
+        <template *ng-outlet="axis.ngTemplateRefs.first; locals: {}"></template>
       </g>
     </svg>
   </svg>
   <!-- not necessary, but useful for debugging -->
   <content></content>`,
-  directives: [ViewDst, NgFor]
+  directives: [NgOutlet, NgFor]
 })
 class Graph {
   width: number;
